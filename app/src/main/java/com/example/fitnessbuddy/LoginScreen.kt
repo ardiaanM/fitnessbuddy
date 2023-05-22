5e01ce73dafd3a084d4bb8fdc839304080ce1e46
ardiaanM <113528715+ardiaanM@users.noreply.github.com>
2023-05-22 14:31:02 +0200

    logout method.

--- app/src/main/java/com/example/fitnessbuddy/LoginScreen.kt
+++ app/src/main/java/com/example/fitnessbuddy/LoginScreen.kt
@@ -5,14 +5,17 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.appcompat.widget.Toolbar
 import android.widget.TextView
+import android.widget.Toast
 import com.example.fitnessbuddy.R
 import com.example.fitnessbuddy.databinding.ActivityLoginScreenBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class LoginScreen : AppCompatActivity() {
 
     private lateinit var loginInfoTV1: TextView
     private lateinit var toolbar: Toolbar
     private lateinit var binding: ActivityLoginScreenBinding
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,7 +28,7 @@ class LoginScreen : AppCompatActivity() {
         supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_home) // Set the home icon
 
         loginInfoTV1 = binding.loginInfoTV1!!
-
+        auth = FirebaseAuth.getInstance()
 
         val email = intent.getStringExtra("email")
 
@@ -35,14 +38,23 @@ class LoginScreen : AppCompatActivity() {
         //only get the name
         loginInfoTV1.text = "Welcome, $name"
 
-
         toolbar.setNavigationOnClickListener {
-            // Navigate back to the LoginActivity when the home icon is clicked
+            // Navigate back to the LoginScreen when the home icon is clicked
             val intent = Intent(this, LoginScreen::class.java)
             startActivity(intent)
             finish()
         }
+
+        binding.buttonLogout.setOnClickListener {
+            // Handle logout button click
+            auth.signOut()
+            Toast.makeText(this, "Logout successful", Toast.LENGTH_SHORT).show()
+
+
+            // Redirect to MainActivity
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
-
 }
-
